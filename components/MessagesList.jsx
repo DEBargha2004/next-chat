47b036161d79715a9efffc76c23279825049f59f
DEBargha2004 <debarghasaha16@gmail.com>
2023-08-19 22:18:56 +0530

    remove
    emoji-mart removed from all the files

--- components/MessagesList.jsx
+++ components/MessagesList.jsx
@@ -5,8 +5,6 @@ import { Appstate } from "@/hooks/context";
 import ChatMessage from "./ChatMessage";
 import RefMessage from "./RefMessage";
 import ChatInputImage from "./ChatInputImage";
-import Picker from '@emoji-mart/react'
-import {data} from '@emoji-mart/data'
 
 function MessagesList({ list,database }) {
   const messagesListRef = useRef(null);
