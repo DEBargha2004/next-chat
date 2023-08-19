3a7722f176d9446fc25fdfe35acb7a8bdf32cae5
DEBargha2004 <debarghasaha16@gmail.com>
2023-08-19 18:03:44 +0530

    fix
    new file created for component OverviewOfLast

---
+++ components/OverviewOfLast.jsx
@@ -0,0 +1,32 @@
+import messageStatus from "@/functions/messageStaus"
+import Image from "next/image"
+
+const OverviewOfLast = ({ message, unread, lastMessage }) => {
+    return (
+      <div className='flex items-center justify-between w-full'>
+        <div className='flex items-center w-[90%]'>
+          {message?.message_type?.image ? (
+            <Image
+              width={16}
+              height={16}
+              src='https://cdn-icons-png.flaticon.com/512/16/16410.png'
+              className='h-4 opacity-60 mr-1'
+              alt='image-icon'
+            />
+          ) : null}
+          {message?.message_type?.text ? (
+            <p className='line-clamp-1'>{message.message_data.text}</p>
+          ) : null}
+          <span className='ml-2'>{messageStatus(lastMessage)}</span>
+        </div>
+  
+        {unread?.length ? (
+          <div className='w-[18px] h-[18px] bg-green-500 rounded-full text-white text-[10px] flex justify-center items-center'>
+            {unread?.length > 99 ? `99+` : unread?.length}
+          </div>
+        ) : null}
+      </div>
+    )
+  }
+
+export default OverviewOfLast
--- components/Userbox.jsx
+++ components/Userbox.jsx
@@ -13,36 +13,8 @@ import { arrayRemove, arrayUnion, doc, updateDoc,getDoc } from 'firebase/firesto
 import { firestoreDB } from '@/firebase.config'
 import { Image } from 'next/image'
 import { ParticipantsOverlay } from './ParticipantsOverlay'
+import OverviewOfLast from './OverviewOfLast'
 
-
-
-const OverviewOfLast = ({ message, unread, lastMessage }) => {
-  return (
-    <div className='flex items-center justify-between w-full'>
-      <div className='flex items-center w-[90%]'>
-        {message?.message_type?.image ? (
-          <Image
-            width={16}
-            height={16}
-            src='https://cdn-icons-png.flaticon.com/512/16/16410.png'
-            className='h-4 opacity-60 mr-1'
-            alt='image-icon'
-          />
-        ) : null}
-        {message?.message_type?.text ? (
-          <p className='line-clamp-1'>{message.message_data.text}</p>
-        ) : null}
-        <span className='ml-2'>{messageStatus(lastMessage)}</span>
-      </div>
-
-      {unread?.length ? (
-        <div className='w-[18px] h-[18px] bg-green-500 rounded-full text-white text-[10px] flex justify-center items-center'>
-          {unread?.length > 99 ? `99+` : unread?.length}
-        </div>
-      ) : null}
-    </div>
-  )
-}
 
 function Userbox ({
   item,
@@ -57,7 +29,6 @@ function Userbox ({
   enableMoreInfo
 }) {
   const { user } = useUser()
-  const router = useRouter()
   const { presenceInfo, messages, selectedGroup } = useContext(Appstate)
   const [showMoreInfo, setShowMoreInfo] = useState(false)
 
